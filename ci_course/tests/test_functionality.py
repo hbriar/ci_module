ed95cf948a094a0c27a54f231051043e5613c8ec
Hannah Briar <255448601+hbriar@users.noreply.github.com>
2026-02-05 16:41:09 +0000

    Fix style errors

--- ci_course/tests/test_functionality.py
+++ ci_course/tests/test_functionality.py
@@ -16,15 +16,4 @@ def test_minimum():
     assert ci_course.minimum(1, 2, 3) == 1
     assert ci_course.minimum(1.2, 2.3) == 1.2
     assert ci_course.minimum(-1.2, -3, 6) == -3
-    assert ci_course.minimum("cat", "box") == None
-
-
-def test_minimum():
-    """
-    Test the function `minimum` in functionality.py
-    """
-    assert ci_course.minimum(1, 2, 3) == 1
-    assert ci_course.minimum(1.2, 2.3) == 1.2
-    assert ci_course.minimum(-1.2, -3, 6) == -3
     assert ci_course.minimum("cat", "box") is None
-    
